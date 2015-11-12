4475c426d5c4a7fd8bdfc12159dea7633561198f
AlessioPz <alessio.papazzoni@gmail.com>
2015-11-12 10:33:55 +0100

    Initial Commit

---
+++ EndpointIosClient/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  EndpointIosClient
+//
+//  Created by Alessio Papazzoni on 12/11/15.
+//  Copyright © 2015 Alessio Papazzoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBOutlet weak var lbl_answer: UILabel!
+    @IBOutlet weak var txt_question: UITextField!
+    @IBAction func connect(sender: AnyObject) {
+        var service : GTLServiceSuggestionBeanApi? = nil
+        if service == nil {
+            service = GTLServiceSuggestionBeanApi()
+            service?.retryEnabled = true
+        }
+        
+        let query : GTLQuerySuggestionBeanApi = GTLQuerySuggestionBeanApi.queryForGetAnswerWithQuestion(txt_question.text)
+        
+        var ticket = GTLServiceTicket()
+        ticket = service!.executeQuery(query, completionHandler: { (ticket: GTLServiceTicket!, object: AnyObject!, error: NSError!) -> Void in
+            
+            print("Analytics: \(object) or \(error)")
+            let resp : GTLSuggestionBeanApiSuggestionBean = object as! GTLSuggestionBeanApiSuggestionBean
+            print("User resp.answer: \(resp.answer)")
+            self.lbl_answer.text = resp.answer
+            
+        })
+    }
+}
+
